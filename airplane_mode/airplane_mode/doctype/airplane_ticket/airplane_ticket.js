fcfe79a0c65078ed365a7c6b5702af1cfcc9c1b1
Aravind Rajendran <aravindarcbe@gmail.com>
2025-10-15 13:34:29 +0530

    Day3- started

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -6,3 +6,31 @@
 
 // 	},
 // });
+frappe.ui.form.on("Airplane Ticket", {
+	refresh(frm) {
+		frm.add_custom_button(
+			"Assign seat",
+			function () {
+				let dialog = new frappe.ui.Dialog({
+					title: "Select seat",
+					fields: [
+						{
+							label: "Seat Number",
+							fieldname: "seat_number",
+							fieldtype: "Data",
+							reqd: true,
+						},
+					],
+					primary_action_label: "Assign",
+					primary_action(values) {
+						frm.set_value("seat", values.seat_number);
+						dialog.hide();
+					},
+				});
+
+				dialog.show();
+			},
+			__("Actions")
+		);
+	},
+});
